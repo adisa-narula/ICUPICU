6a0634b87b942e51b51e272a87dd299015413901
Adisa Narula <an1519@nyu.edu>
2016-11-20 17:36:41 -0500

    finished homepage

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -1,7 +1,18 @@
+$.getJSON("data/information.json", function(data)
+{
+    $("#welcome").append("Welcome " + data.first_name + " " + data.last_name);
+});
+
+$('.speech-form').on('submit', function () {
+    var speech = $('#speech_text').val();
+    speak(speech);
+    return false;
+});
+
 function speak(message) {
   console.log(message);
   var msg = new SpeechSynthesisUtterance(message);
-  msg.rate = 0.2;
+  msg.rate = 1;
   window.speechSynthesis.speak(msg);
 
   var voices = window.speechSynthesis.getVoices();
