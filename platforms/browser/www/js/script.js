fb8551426174574fb44bbc7fa126293e93934264
Adisa Narula <an1519@nyu.edu>
2016-11-20 18:59:52 -0500

    added calling nurse functionality

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -9,12 +9,24 @@ $('.speech-form').on('submit', function () {
     return false;
 });
 
+function callNurse() {
+  speak("Calling nurse");
+  // location.href = '../index.html'
+  $( "#content" ).fadeOut( "fast");
+  $( "#call_nurse" ).show( "slow", function() {
+    $("#nurse_button").attr("aria-hidden","true");
+    $(this).delay(5000).fadeOut("slow", function() {
+      $("#content").show();
+      $("#nurse_button").attr("aria-hidden","false");
+    });
+ });
+}
+
 function speak(message) {
   console.log(message);
   var msg = new SpeechSynthesisUtterance(message);
   msg.rate = 1;
   window.speechSynthesis.speak(msg);
-
   var voices = window.speechSynthesis.getVoices();
-  console.log(voices);
+  // console.log(voices);
 }
