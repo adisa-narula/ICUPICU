5dda047489eaf64f873d6f0c6136cd4eff2aa150
Adisa Narula <an1519@nyu.edu>
2016-11-21 10:19:07 -0500

    added pain scale

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,6 @@
+var body_part;
+var scale;
+
 $.getJSON("data/information.json", function(data)
 {
     $("#welcome").append("Welcome " + data.first_name + " " + data.last_name);
@@ -13,19 +16,33 @@ function callNurse() {
   speak("Calling nurse");
   // location.href = '../index.html'
   $( "#content" ).fadeOut( "fast");
-  $( "#call_nurse" ).show( "slow", function() {
+  $( "#call_nurse" ).show(1000, function() {
     $("#nurse_button").attr("aria-hidden","true");
     $(this).delay(5000).fadeOut("slow", function() {
-      $("#content").show();
+      $("#content").fadeIn();
       $("#nurse_button").attr("aria-hidden","false");
     });
  });
 }
 
+function pain(part) {
+  body_part = part;
+  speak("My " + body_part + " hurts");
+  $( "#content" ).fadeOut("fast");
+  $("#pain_scale").show(1000);
+}
+
+function painScale(number) {
+  scale = number;
+  speak(scale);
+  console.log(body_part);
+  console.log(number);
+}
+
 function speak(message) {
   console.log(message);
   var msg = new SpeechSynthesisUtterance(message);
-  msg.rate = 1;
+  msg.rate = 0.8;
   window.speechSynthesis.speak(msg);
   var voices = window.speechSynthesis.getVoices();
   // console.log(voices);
