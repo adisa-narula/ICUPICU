30ada13fb8cb2020f9044edb34dfdb7a5adcccf4
Adisa Narula <an1519@nyu.edu>
2016-11-21 20:49:43 -0500

    added pain functionalities

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -1,5 +1,4 @@
 var body_part;
-var scale;
 
 $.getJSON("data/information.json", function(data)
 {
@@ -13,9 +12,14 @@ $('.speech-form').on('submit', function () {
 });
 
 function callNurse() {
+
+  console.log("calling nurse");
+
   speak("Calling nurse");
   // location.href = '../index.html'
-  $( "#content" ).fadeOut( "fast");
+  $("#content").fadeOut( "fast");
+  $("#pain_scale").fadeOut("fast");
+  $("#pain_panel").fadeOut("fast");
   $( "#call_nurse" ).show(1000, function() {
     $("#nurse_button").attr("aria-hidden","true");
     $(this).delay(5000).fadeOut("slow", function() {
@@ -25,22 +29,46 @@ function callNurse() {
  });
 }
 
+function cancelPain(cancel) {
+  console.log("Canceling " + cancel);
+  $("#pain_scale").fadeOut("fast", function() {
+    $("#pain_panel").fadeOut("fast", function() {
+      $("#content").fadeIn();
+    });
+  });
+}
+
 function pain(part) {
+  console.log("pain: " + part);
   body_part = part;
-  speak("My " + body_part + " hurts");
-  $( "#content" ).fadeOut("fast");
-  $("#pain_scale").show(1000);
+  $( "#content" ).fadeOut("fast", function() {
+    $("#pain_panel").fadeOut("fast", function() {
+        $("#pain_scale").show(1000);
+    });
+  });
 }
 
 function painScale(number) {
-  scale = number;
-  speak(scale);
-  console.log(body_part);
-  console.log(number);
+  console.log("pain scale");
+  var scaleID = "#pain"+number;
+  $("#pain_scale").fadeOut("fast", function() {
+    var pain = "<br><br><h1> "+ body_part + " Pain </h1>";
+    var scale = "<br><h1> Scale: " + "</h1><br><br>";
+    $("#pain_headers").empty();
+    $("#pain_headers").append(pain);
+    $("#pain_headers").append(scale);
+    $(scaleID).show("fast");
+    $("#pain_panel").show(1000, function(){
+      speak("My " + body_part + " hurts and the pain scale is " + number);
+      $(this).delay(4000).fadeOut("slow", function() {
+        $(scaleID).hide();
+        $("#content").fadeIn();
+      });
+    });
+  });
 }
 
 function speak(message) {
-  console.log(message);
   var msg = new SpeechSynthesisUtterance(message);
   msg.rate = 0.8;
   window.speechSynthesis.speak(msg);
